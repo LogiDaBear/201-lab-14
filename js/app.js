31afe7d97b18b944afb964abdce06d69bbdee53a
Logan Reese <lreese2112@gmail.com>
2023-02-23 21:38:57 +0000

    added audreys and some other

--- js/app.js
+++ js/app.js
@@ -13,7 +13,9 @@ const Cart = function(items) {
 };
 
 Cart.prototype.addItem = function(product, quantity) {
-  // TODO: Fill in this instance method to create a new CartItem and add it to this.items
+  // Done: Fill in this instance method to create a new CartItem and add it to this.items
+  let newItem = new CartItem(product, quantity);
+  this.items.push(newItem)
 };
 
 Cart.prototype.saveToLocalStorage = function() {
@@ -23,6 +25,8 @@ Cart.prototype.saveToLocalStorage = function() {
 Cart.prototype.removeItem = function(item) {
   // TODO: Fill in this instance method to remove one item from the cart.
   // Note: You will have to decide what kind of parameter to pass in here!
+  this.removeItem = product;
+  this.removeItem = item
 };
 
 Cart.prototype.updateCounter = function() {
